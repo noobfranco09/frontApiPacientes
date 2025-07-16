41f2eacba2517f1d34d3432dd5f67d912695f2a7
noobfranco <noobfranco09@gmail.com>
2025-07-16 16:45:31 -0500

    se termina el front

--- assets/js/getAllPacientes.js
+++ assets/js/getAllPacientes.js
@@ -11,15 +11,19 @@ document.addEventListener("DOMContentLoaded", () => {
     crearPaciente();
   });
 
-    const btnEditarPaciente = document.querySelector("#btnEditarPaciente");
-    btnEditarPaciente.addEventListener('click', (e) => {
-        e.preventDefault();
-        apiEditarPaciente();
-     });
-
-  btnCrearPaciente.addEventListener("click", (e) => {
+  const btnEditarPaciente = document.querySelector("#btnModalEditarPaciente");
+  btnEditarPaciente.addEventListener("click", (e) => {
     e.preventDefault();
-    editarPaciente();
+    apiEditarPaciente();
+  });
+  const btnBuscar = document.querySelector("#btnBuscar");
+  btnBuscar.addEventListener("click", (e) => {
+    e.preventDefault();
+    buscarPorId();
+  });
+  const btnVerpacientes = document.querySelector('#verPacientes');
+  btnVerpacientes.addEventListener('click', () => { 
+    mostrarPacientes();
   });
 });
 
@@ -32,7 +36,8 @@ async function getAllPacientes() {
         const cuerpoTablaPacientes = document.querySelector(
           "#cuerpoTablaPacientes"
         );
-        res.forEach((paciente) => {
+        cuerpoTablaPacientes.innerHTML = "";
+        res.data.forEach((paciente) => {
           let tr = document.createElement("tr");
           let btnEditar = document.createElement("button");
           btnEditar.setAttribute(
@@ -40,6 +45,7 @@ async function getAllPacientes() {
             "btn btn-primary btnEditarPaciente m-1"
           );
           let dataPaciente = {
+            id: paciente.id,
             name: paciente.name,
             age: paciente.age,
             gender: paciente.gender,
@@ -88,8 +94,10 @@ function gestionPacientes(e) {
 
   if (target.classList.contains("btnEditarPaciente")) {
     const id = target.getAttribute("data-id");
-    const dataPaciente =JSON.parse(target.getAttribute("data-paciente"));
-    llenarEditarPaciente(id, dataPaciente), dataPaciente;
+    const dataPaciente = target.getAttribute("data-paciente");
+    console.log(dataPaciente);
+    console.log(id);
+    llenarEditarPaciente(id, dataPaciente);
   } else if (target.classList.contains("btnEliminarPaciente")) {
     const id = target.getAttribute("data-id");
     eliminarPaciente(id);
@@ -97,16 +105,22 @@ function gestionPacientes(e) {
 }
 
 async function llenarEditarPaciente(id, dataPaciente) {
-    try {
-    document.querySelector("#editarNombrePaciente").value=dataPaciente.name;
-    document.querySelector("#editarEdadPaciente").value=dataPaciente.age;
-    document.querySelector("#editarGeneroPaciente").value=dataPaciente.gender;
-    document.querySelector("#editarDiagnosticoPaciente").value=dataPaciente.diagnosis;
-    document.querySelector("#editarFechaIngresoPaciente").value=dataPaciente.created_at;
-  } catch (error)
-  {
-      console.log(error);
-   }
+  try {
+    const datos = JSON.parse(dataPaciente);
+    document.querySelector("#idModalEditarPaciente").value = id;
+    document.querySelector("#editarNombrePaciente").value = datos.name;
+    document.querySelector("#editarEdadPaciente").value = datos.age;
+    document.querySelector("#editarDiagnosticoPaciente").value =
+      datos.diagnosis;
+    document.querySelector("#editarFechaIngresoPaciente").value =
+      datos.created_at;
+    const selectGenero = document.querySelector("#editarGeneroPaciente");
+    const generoPaciente = document.querySelector(
+      "#editarGeneroPaciente"
+    ).value;
+  } catch (error) {
+    console.log(error);
+  }
 }
 
 async function eliminarPaciente(id) {
@@ -145,7 +159,117 @@ async function crearPaciente() {
     });
 }
 
-async apiEditarPaciente()
+async function apiEditarPaciente() {
+  let id = document.querySelector("#idModalEditarPaciente").value;
+  let nombreEditado = document.querySelector("#editarNombrePaciente").value;
+  let edadEditado = document.querySelector("#editarEdadPaciente").value;
+  let generoEditado = document.querySelector("#editarGeneroPaciente").value;
+  let diagnosticoEditado = document.querySelector(
+    "#editarDiagnosticoPaciente"
+  ).value;
+  let fechaIngresoeditado = document.querySelector(
+    "#editarFechaIngresoPaciente"
+  ).value;
+  const pacienteEditado = {
+    name: nombreEditado,
+    age: edadEditado,
+    gender: generoEditado,
+    diagnosis: diagnosticoEditado,
+    created_at: fechaIngresoeditado,
+  };
+  try {
+    await fetch(`${urlApi}update.php?id=${id}`, {
+      method: "PUT",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(pacienteEditado),
+    }).then((res) => {
+      if (res.ok) {
+        console.log("Editado con éxito");
+      } else {
+        throw new Error(res);
+      }
+      window.location.reload();
+    });
+  } catch (e) {
+    console.log(e);
+  }
+}
+
+async function buscarPorId() {
+  const id = document.querySelector("#buscar").value;
+  if (id.length > 0) {
+    try {
+      await fetch(`${urlApi}get_one.php?id=${id}`)
+        .then((res) => res.json())
+        .then((res) => {
+          const cuerpoTablaPacientes = document.querySelector(
+            "#cuerpoTablaPacientes"
+          );
+          console.log(res);
+          const paciente = res;
+          cuerpoTablaPacientes.innerHTML = "";
+
+            let tr = document.createElement("tr");
+            let btnEditar = document.createElement("button");
+            btnEditar.setAttribute(
+              "class",
+              "btn btn-primary btnEditarPaciente m-1"
+            );
+            let dataPaciente = {
+              id: paciente.id,
+              name: paciente.name,
+              age: paciente.age,
+              gender: paciente.gender,
+              diagnosis: paciente.diagnosis,
+              created_at: paciente.created_at,
+            };
+            btnEditar.appendChild(document.createTextNode("Editar"));
+            btnEditar.setAttribute("data-id", `${paciente.id}`);
+            btnEditar.setAttribute(
+              "data-paciente",
+              JSON.stringify(dataPaciente)
+            );
+            btnEditar.setAttribute("data-bs-toggle", "modal");
+            btnEditar.setAttribute("data-bs-target", "#editarPaciente");
+            let btnEliminar = document.createElement("button");
+            btnEliminar.setAttribute(
+              "class",
+              "btn btn-danger btnEliminarPaciente m-1"
+            );
+            btnEliminar.appendChild(document.createTextNode("Eliminar"));
+            btnEliminar.setAttribute("data-id", `${paciente.id}`);
+
+            let td1 = document.createElement("td");
+            let td2 = document.createElement("td");
+            let td3 = document.createElement("td");
+            let td4 = document.createElement("td");
+            let td5 = document.createElement("td");
+            let td6 = document.createElement("td");
+            let td7 = document.createElement("td");
+
+            td1.appendChild(document.createTextNode(paciente.id));
+            td2.appendChild(document.createTextNode(paciente.name));
+            td3.appendChild(document.createTextNode(paciente.age));
+            td4.appendChild(document.createTextNode(paciente.gender));
+            td5.appendChild(document.createTextNode(paciente.diagnosis));
+            td6.appendChild(document.createTextNode(paciente.created_at));
+            td7.append(btnEditar, btnEliminar);
+            tr.append(td1, td2, td3, td4, td5, td6, td7);
+
+            cuerpoTablaPacientes.appendChild(tr);
+
+        });
+    } catch (error) {
+      console.log(error + " error al traer los pacientes");
+    }
+  } else {
+    console.log("No se ha dado ningún id");
+  }
+}
+
+function mostrarPacientes()
 {
-    
+  getAllPacientes();
  }
